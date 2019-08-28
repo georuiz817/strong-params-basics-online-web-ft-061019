dbe66bedd2de343e54601ba2e953b79d1d7e1c26
George Ruiz <georuiz817@gmail.com>
2019-08-28 16:20:10 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,6 @@ end
  
 def update
   @post = Post.find(params[:id])
-  @post.update(params.require(:post).permit(:title))
-  redirect_to post_path(@post)
-end
 
 	def edit
 	  @post = Post.find(params[:id])
